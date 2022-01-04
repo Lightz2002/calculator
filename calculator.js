3e647de0c5a3afbb992ca9d24fcb302291102e62
Ryan <kenidyryan@gmail.com>
2022-01-04 09:52:11 +0700

    initial commit

---
+++ calculator.js
@@ -0,0 +1,11 @@
+let result = document.querySelector(".main-result");
+
+result.textContent = 0;
+
+/* 
+add click event for icon
+if the icon clicked has fa-sun class, set the background to silver
+give the animation of slideInRight
+if the icon clicked has fa-moon class, set the background to black
+
+*/
